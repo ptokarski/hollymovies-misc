8a0c465834dba89c9eee7f2289d8c6e445ebdf49
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-12-09 21:47:03 +0100

    Add migrations.

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,7 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from hollymovies.models import ModelBase
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 
@@ -17,7 +17,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = ModelBase.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
