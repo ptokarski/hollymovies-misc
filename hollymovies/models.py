3d82cc8c72ef4f8f34da9e6e2a6b912ce1ac2a6a
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-12-09 20:01:48 +0100

    Add genre model.

---
+++ hollymovies/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+ModelBase = declarative_base()
+
+
+class Genre(ModelBase):
+    __tablename__ = 'genre'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+
+ENGINE = create_engine('sqlite:///db.sqlite3')
+Session = sessionmaker(bind=ENGINE)
