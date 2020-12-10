f0dd6bcfb2c10adbd2a0c71b6cb4a87e5659f5ff
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-12-10 23:14:28 +0100

    Add movie model.

--- hollymovies/models.py
+++ hollymovies/models.py
@@ -1,8 +1,11 @@
 from contextlib import contextmanager
+from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import (
+    Column, Date, DateTime, ForeignKey, Integer, String, create_engine
+)
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship, sessionmaker
 
 models_registry = {}
 ModelBase = declarative_base(class_registry=models_registry)
@@ -12,6 +15,19 @@ class Genre(ModelBase):
     __tablename__ = 'genre'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
+    movies = relationship('Movie', back_populates='genre')
+
+
+class Movie(ModelBase):
+    __tablename__ = 'movie'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(length=128), nullable=False)
+    genre_id = Column(Integer, ForeignKey('genre.id'), nullable=False)
+    genre = relationship('Genre', back_populates='movies')
+    rating = Column(Integer, nullable=False)
+    released = Column(Date, nullable=False)
+    description = Column(String)
+    created = Column(DateTime, default=datetime.utcnow)
 
 
 def all_models():
