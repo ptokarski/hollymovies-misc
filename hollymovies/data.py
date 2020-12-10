7b6d72742155bb2dba8a4b53784d11f09ae13d09
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-12-10 22:29:52 +0100

    Add function for loaddata.

--- hollymovies/data.py
+++ hollymovies/data.py
@@ -1,6 +1,7 @@
 import json
 
 from hollymovies import models
+from hollymovies.models import all_models
 
 
 def _to_dict(columns, entry):
@@ -21,5 +22,19 @@ def _dump_model(output_file, session, model):
 
 def dumpdata(output_path):
     with open(output_path, 'w') as output_file, models.session() as session:
-        for model in models.all_models():
+        for model in all_models():
             _dump_model(output_file, session, model)
+
+
+def _objects_to_add(table_to_model, input_file):
+    for line in input_file:
+        entry = json.loads(line)
+        model = table_to_model[entry['model']]
+        yield model(**entry['fields'])
+
+
+def loaddata(input_path):
+    with open(input_path) as input_file, models.session() as session:
+        table_to_model = {model.__tablename__: model for model in all_models()}
+        objects = _objects_to_add(table_to_model, input_file)
+        session.bulk_save_objects(objects)
