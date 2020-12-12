1dced946d77f78b0b6355a0dedaa625cb22a1c02
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-12-12 09:05:16 +0100

    Add reading url arguments.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, world!'
+@app.route('/<s0>')
+def hello(s0):
+    s1 = request.args['s1']
+    return f'Hello, {s0} and {s1} world!'
 
 
 if __name__ == "__main__":
