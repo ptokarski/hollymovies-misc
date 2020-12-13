eed3db5674503660d1479f75574282c34b159f6d
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-12-13 23:11:23 +0100

    Migrate to simple CBV.

--- hollymovies/views.py
+++ hollymovies/views.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, render_template
+from flask.views import MethodView
 
 from hollymovies.models import Movie
 
 main_blueprint = Blueprint('main', __name__)
 
 
-@main_blueprint.route('/')
-def movies():
-    return render_template('movies.html', movies=Movie.query)
+class MovieListView(MethodView):
+    def get(self):
+        return render_template('movies.html', movies=Movie.query)
+
+
+main_blueprint.add_url_rule('/', view_func=MovieListView.as_view('index'))
