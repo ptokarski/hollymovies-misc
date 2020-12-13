35ec4af8942c3d7ebc80a44e7f0aefcee4e250e5
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-12-13 19:03:51 +0100

    Use flask-sqlalchemy.

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from sqlalchemy.orm import scoped_session
 
-from hollymovies.models import ModelBase, Session
+from hollymovies.models import db
 from hollymovies.views import main_blueprint
-
-db_session = scoped_session(Session)
-ModelBase.query = db_session.query_property()
 
 app = Flask(__name__)
 app.register_blueprint(main_blueprint)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 Bootstrap(app)
-
-
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    db_session.remove()
 
 
 if __name__ == "__main__":
