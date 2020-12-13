39976ee9bca0967c12bd286ae820fe08f0d1fa4a
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-12-13 21:21:45 +0100

    Add dumpdata and loaddata to app.py.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import click
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 
+from hollymovies.data import dumpdata, loaddata
 from hollymovies.models import db
 from hollymovies.views import main_blueprint
 
@@ -12,6 +14,26 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 Migrate(app, db)
 Bootstrap(app)
+
+
+_DUMPDATA_HELP = (
+    'Dumps data from the database as json lines to provided file path.'
+)
+
+
+@app.cli.command('dumpdata', help=_DUMPDATA_HELP)
+@click.option('--output-file', type=click.File('w'), help='Output file name.')
+def dumpdata_command(output_file):
+    dumpdata(output_file)
+
+
+_LOADDATA_HELP = 'Loads data from provided json-lines file to the database.'
+
+
+@app.cli.command('loaddata', help=_LOADDATA_HELP)
+@click.option('--input-file', type=click.File('r'), help='Input file name.')
+def loaddata_command(input_file):
+    loaddata(input_file)
 
 
 if __name__ == "__main__":
